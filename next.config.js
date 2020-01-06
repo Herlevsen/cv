377f3e47a9131b8364b11eea9da470bcfc6105c0
Jens Herlevsen <jensherlevsen@gmail.com>
2020-01-07 00:29:44 +0100

    Change next js asset prefix

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const withCSS = require('@zeit/next-css')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withCSS({
-  assetPrefix: isProd ? 'https://herlevsen.github.io/cv/docs' : '',
+  assetPrefix: isProd ? 'https://herlevsen.github.io/cv' : '',
 })
